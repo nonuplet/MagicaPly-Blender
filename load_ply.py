54c4da9a65f92df6e7b10a807d47118943bb094d
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2024-01-18 22:16:40 +0900

    Add decimate and origin option

--- load_ply.py
+++ load_ply.py
@@ -5,7 +5,7 @@ from bpy.props import BoolProperty, CollectionProperty, IntProperty, StringPrope
 from bpy.types import Context, Operator, OperatorFileListElement
 from bpy_extras.io_utils import ImportHelper
 
-from . import bake_texture, setup_model, unwrap_uv
+from . import bake_texture, setup_model, unwrap_uv, decimate_model, set_origin
 
 
 class MPLoadPly(Operator, ImportHelper):
@@ -40,16 +40,40 @@ class MPLoadPly(Operator, ImportHelper):
         name="Texture Resolution", description="texture resolution for baking", default=512
     )
 
+    decimate: BoolProperty(
+        name="Apply Decimate", description="Apply Decimate Modifier", default=False
+    )
+
+    origin_bottom: BoolProperty(
+        name="Set Bottom as Origin", description="Set the bottom of model as origin", default=False
+    )
+
     def execute(self, context: Context):
         paths = [os.path.join(self.directory, name.name) for name in self.files]
 
         for path in paths:
-            import_ply(path, self.auto_setup, self.merge_vertices, self.bake, self.resolution)
+            import_ply(
+                path,
+                self.auto_setup,
+                self.merge_vertices,
+                self.bake,
+                self.resolution,
+                self.decimate,
+                self.origin_bottom,
+            )
 
         return {"FINISHED"}
 
 
-def import_ply(path: str, setup: bool, merge_vertices: bool, bake: bool, resolution: int):
+def import_ply(
+    path: str,
+    setup: bool,
+    merge_vertices: bool,
+    bake: bool,
+    resolution: int,
+    decimate: bool,
+    origin_bottom: bool,
+):
     try:
         name = os.path.splitext(os.path.basename(path))[0]
         print("path: " + path)
@@ -65,6 +89,12 @@ def import_ply(path: str, setup: bool, merge_vertices: bool, bake: bool, resolut
 
         if bake:
             bake_texture.auto_bake(obj, resolution)
+
+        if decimate:
+            decimate_model.decimate()
+
+        if origin_bottom:
+            set_origin.set_origin(obj)
 
     except Exception as e:
         print(e)
